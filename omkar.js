b571efe6947dcec743685c88502562bda77f5ae3
Tanya Chauhan <tanyachauhan0628@gmail.com>
2026-04-06 14:55:19 +0530

    Update omkar.js

--- omkar.js
+++ omkar.js
@@ -4,7 +4,7 @@ function sendMessage() {
   let email = document.getElementById("email").value;
   let message = document.getElementById("message").value;
 
-  let whatsappNumber = "918368391336"; // your number
+  let whatsappNumber = "918800794368"; // your number
 
   let url = "https://wa.me/" + whatsappNumber + "?text=" +
     encodeURIComponent(
